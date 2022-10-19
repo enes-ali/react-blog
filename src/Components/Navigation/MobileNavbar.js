1d4b0d09f4206993398a3ddc6bc28a3e137a501a
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-19 22:31:56 +0300

    Refactor MobileNavbar to functional component

--- src/Components/Navigation/MobileNavbar.js
+++ src/Components/Navigation/MobileNavbar.js
@@ -4,34 +4,13 @@ import "../../Styles/Navigation/MobileNavbar.css";
 import NavbarMenu from "./NavMenu";
 
 
-class MobileNavbar extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            on: false,
-        };
-    }
+export default function MobileNavbar(props){
 
-    toggleMenu(){
-        this.setState({
-            on: !this.state.on,
-        });
-    }
-
-    onMenuItemClick(idx){
-        this.toggleMenu();
-        console.log(`clicked ${idx}`)
-    }
-
-    render(){
-        return(
-            <div id="mobile-navbar">
-                <NavBrand />
-                <NavbarMenu direction="row"
-                    items={ {"Sign up": "/sign-up"} }/>
-            </div>  
-        );
-    }
+    return(
+        <div id="mobile-navbar">
+            <NavBrand />
+            <NavbarMenu direction="row"
+                items={ {"Sign up": "/sign-up"} }/>
+        </div>
+    );
 }
-
-export default MobileNavbar;
