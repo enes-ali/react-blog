dbd8ebe474bdb9c418eb1591a822e22988cb5fdf
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-19 16:11:39 +0300

    ToggleButton documentation

--- src/Components/Buttons/ToggleButton.js
+++ src/Components/Buttons/ToggleButton.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "../../Styles/Buttons/ToggleButton.css";
 
+/*
+[Component]
+ToggleButton:
+    @desc: A simple toggle button that changes its state every time you click
+    it
+
+    @props:
+        -toggled: Initial state of the button
+        -icon: Icon to show when the button is not toggled
+        -toggled_icon: Icon to show when the button is toggled
+        -text: Text to show when the button is not toggled
+        -toggled_text: text to show when the button is toggled
+        -onClick(): Callback to call when a click event happens
+*/
 
 export default function ToggleButton(props){
     let [toggled, toggle] = useState(false);
