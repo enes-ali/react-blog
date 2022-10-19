80c1a9b81f5420e52aad9c42ef782d9d5e265cdc
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-19 15:42:57 +0300

    Refactor ToggleButton component to use hooks instead of class component

--- src/Components/Buttons/ToggleButton.js
+++ src/Components/Buttons/ToggleButton.js
@@ -1,46 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../Styles/Buttons/ToggleButton.css";
 
 
-class ToggleButton extends React.Component{
+export default function ToggleButton(props){
+    let [toggled, toggle] = useState(false);
 
-    constructor(props){
-        super(props);
-        this.state = {
-            checked: this.props.checked,
-        };
-    }
+    useEffect(() => { toggle(props.checked); }, [props.checked]);
 
-    componentDidUpdate(prevProps, prevState){
-        if(this.props.checked != prevProps.checked){
-            this.setState({checked: this.props.checked});
-        }
-    }
+    const icon = () => toggled ? "fa-x" : "fa-bars";
 
-    handleClick(event){
-        this.setState({ checked: !this.state.checked });
-
-        this.props.onClick();
-    }
-
-    iconClass(){
-        return this.state.checked ? "fa-x" : "fa-bars";
-    }
-
-    text(){
-        if(!this.props.text || !this.props.text_active)
+    const text = () => {
+        if(!props.text || !props.text_active)
             return
-        return <span>{this.state.checked ? this.props.text_active : this.props.text}</span>;
+        return <span>{toggled ? props.text_active : props.text}</span>;
     }
 
-    render(){
-        return(
-            <button className="toggle-button" onClick={ this.handleClick.bind(this) }>
-                <i className={ `fa-solid ${this.iconClass()}` }></i>
-                {this.text()}
-            </button>
-        );
+    const onClick = () => {
+        props.onClick();
+        toggle(!toggled);
     }
+
+    return(
+        <button className="toggle-button" onClick={ onClick }>
+            <i className={ `fa-solid ${icon()}` }></i>
+            {text()}
+        </button>
+    );
 }
-
-export default ToggleButton;
