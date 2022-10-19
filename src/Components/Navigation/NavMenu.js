b33dc5ec9055632e282fde42762d5fc3ec1d8bd4
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-19 22:32:43 +0300

    Delete unused variables from NavMenu component

--- src/Components/Navigation/NavMenu.js
+++ src/Components/Navigation/NavMenu.js
@@ -2,8 +2,7 @@ import NavList from "../Lists/NavList";
 
 
 function NavbarMenu(props){
-    let items = {Categories: "/", Write: "/write", "Sign in": "/help", "Sign Up": "/about"};
-
+    
     return(
         <nav id="nav-menu" className={ props.custom_class }>
             <NavList items={ props.items } direction={ props.direction } onItemClick={ props.onItemClick }/>
