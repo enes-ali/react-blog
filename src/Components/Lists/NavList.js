8a0f6dd4546a3f64af7404d9500e5b1d4ac9a1ae
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-19 22:42:37 +0300

    NavList documentation

--- src/Components/Lists/NavList.js
+++ src/Components/Lists/NavList.js
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
 import "../../Styles/Lists/Lists.css";
 
-function NavList(props){
+/*
+[Component]
+NavList:
+    @desc: A reusable unordered list to be used for navigation
+
+    @props:
+        -direction: Direction of the list.  
+            "col": vertical list
+            "row": horizontal list
+        -items: An object which will be used to create actual list items.
+        keys are used as item text, values are used as links
+*/
+
+export default function NavList(props){
     let menu = Object.entries(props.items).map((element, idx) => {
         return(
             <li key={idx} className="nav-list-i" onClick={ (e) => props.onItemClick(idx) }>
@@ -18,5 +31,3 @@ function NavList(props){
         </ul>
     );
 }
-
-export default NavList;
--- src/Components/Navigation/NavMenu.js
+++ src/Components/Navigation/NavMenu.js
@@ -2,13 +2,12 @@ import NavList from "../Lists/NavList";
 
 
 function NavbarMenu(props){
-    
+
     return(
         <nav id="nav-menu" className={ props.custom_class }>
             <NavList items={ props.items } direction={ props.direction } onItemClick={ props.onItemClick }/>
         </nav>
     );
 }
-
 
 export default NavbarMenu;
