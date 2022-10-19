12641bdd308d37aa3f526ff1173fbfd4898f25a3
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-19 22:30:05 +0300

    Refactor Navbar component to functional component

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -3,21 +3,16 @@ import "../../Styles/Navigation/Navbar.css"
 import NavBrand from "./NavBrand.js";
 import NavMenu from "./NavMenu.js";
 
-class Navbar extends React.Component{
-    nav_menu_items = {Categories: "/", Write: "/write", "Sign in": "/help", "Sign Up": "/about"};
 
-    render(){
-        return(
-            <div id="navbar-wrapper">
-                <div id="navbar">
-                    <NavBrand />
-                    <NavMenu items={this.nav_menu_items} direction="row"/>
-                </div>
+export default function Navbar(){
+    let nav_menu_items = {Categories: "/", Write: "/write", "Sign in": "/help", "Sign Up": "/about"};
+
+    return(
+        <div id="navbar-wrapper">
+            <div id="navbar">
+                <NavBrand />
+                <NavMenu items={ nav_menu_items } direction="row"/>
             </div>
-        );
-    }
-
+        </div>
+    );
 }
-
-
-export default Navbar;
