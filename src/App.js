5ec2f0d5fec456cae2f9cb0f47e3383ad168b083
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-20 21:54:57 +0300

    Refactor App component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import { Component } from "react";
-import { useSelector, connect } from "react-redux";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Components/Navigation/Navbar.js";
 import MobileNavbar from "./Components/Navigation/MobileNavbar.js";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 
-class App extends Component{
-  constructor(props){
-    super(props);
+export default function App(){
+  const mq_small = window.matchMedia("(max-width: 512px)");
+  
+  const [is_mobile, setIsMobile] = useState(mq_small.matches);
 
-    this.media_query = window.matchMedia("(min-width: 768px)")
+  useEffect(() => {
+    mq_small.addEventListener("change", () => { setIsMobile(mq_small.matches) });
+  });
 
-    this.state = {
-      navbar: this.media_query.matches ? <Navbar /> : <MobileNavbar />,
-    }
-  }
-
-  handleMediaChange(event){
-    this.setState({navbar: this.media_query.matches ? <Navbar /> : <MobileNavbar />,});
-  }
-
-  componentDidMount(){
-    this.media_query.addEventListener("change", this.handleMediaChange.bind(this));
-  }
-
-  render(){
-    return(
-      <BrowserRouter>
-        { this.state.navbar }
-      </BrowserRouter>
-    );
-  }
-};
-
-export default App;
+  return(
+    <BrowserRouter>
+        { is_mobile ? <MobileNavbar /> : <Navbar /> }
+    </BrowserRouter>
+  );
+}
