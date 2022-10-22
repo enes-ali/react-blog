48c06f7f7789732f9fd9abb7b83d907a18ab1457
nearFuture00 <enes.ali.karkobak@gmail.com>
2022-10-23 00:14:57 +0300

    Add an animated image to banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffec } from "react";
 import RoundButton from "./Buttons/RoundButton";
-
+import BannerImage from "./BannerImage";
 
 
 export default function Banner(){
     const mq_small = window.matchMedia("(max-width: 520px)");
+    const mq_medium = window.matchMedia("(min-width: 70rem)");
 
     const [isMobile, setIsMobile] = useState(mq_small.matches);
+    const [isMedium, setIsMedium] = useState(mq_medium.matches);
 
     mq_small.addEventListener("change", () => { setIsMobile(mq_small.mathces); })
+    mq_medium.addEventListener("change", () => { setIsMedium(mq_medium.matches) });
 
     return(
         <div id="banner">
@@ -18,6 +21,8 @@ export default function Banner(){
                 stay up to date with new information</p>
                 <RoundButton text="Start exploring" />
             </section>
+            { isMedium ? <BannerImage /> : null}
+            
         </div>
     );
 }
